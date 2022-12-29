e6b1ca16ed0a75d66bd84d4f2288ae4722581781
Cake <31402987+ThatOneCakeGuy@users.noreply.github.com>
2022-12-29 17:36:45 -0600

    Delete scripts directory

--- scripts/spd.sh
+++
@@ -1,6 +0,0 @@
-#Tests speeds on remote linux server
-function spd () {
-echo -e "\nPlease login to test internet speeds for $1"
-ssh root@$1 'echo ; curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -|grep "Download\|Upload"'
-echo
-}
